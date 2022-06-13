e9bf4098c3869071e1020ea90429343fdb502808
Kery-Long <kerydevs@gmail.com>
2022-06-13 15:25:33 -0600

    included signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,50 @@
-import './signup.css'
+import "./signup.css";
 
-import React from 'react'
+import React, { useState } from "react";
 
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setathumbnail] = useState(null);
   return (
-    <div>
-        SignUp
-        </div>
-  )
+    <form className="auth-form">
+      <h2> Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        <span>display name:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>avatar:</span>
+        <input required type="file" />
+      </label>
+      <button className="btn">Signup </button>
+    </form>
+  );
 }

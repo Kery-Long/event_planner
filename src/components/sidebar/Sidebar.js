fb9e66e5dcab670343cad998c01b85527cad4f6a
Kery-Long <kerydevs@gmail.com>
2022-06-14 16:59:52 -0600

    created avatar component and display it conditionally

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,16 +2,23 @@ import "./sidebar.css";
 
 import DashboardIcon from "../../assets/dashboard_icon.svg";
 import AddIcon from "../../assets/add_icon.svg";
+import Avatar from "../avatar/Avatar";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 import React from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Sidebar() {
+
+  const { user} = useAuthContext()
   return (
     <div className="sidebar">
       <div className="sidebar-content">
-        <div className="user"></div>
-        <p>Hey User :)</p>
+        <div className="user">
+          <Avatar src={user.photoURL} />
+          <p>Hi {user.displayName}</p>
+        </div>
+        
         <nav className="links">
           <ul>
             <li>

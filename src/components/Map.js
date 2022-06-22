f502eeb3f519c4cb2541aed693d049158300f109
Kery <kery@Kerys-MacBook-Pro.local>
2022-06-22 14:26:46 -0600

    base app with no extras

---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div>Map
+
+AIzaSyDJ6hj6PCaZ9tJn-H39o_ko_ipm4rPi0Ac
+    </div>
+  )
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWzUIleGV8GIMw8hTbZxLN4Ir2Lwnl9Y",
+  authDomain: "the-dojo-bbd1b.firebaseapp.com",
+  projectId: "the-dojo-bbd1b",
+  storageBucket: "the-dojo-bbd1b.appspot.com",
+  messagingSenderId: "232617972594",
+  appId: "1:232617972594:web:bf562bea62211ee4ce43eb",
+  measurementId: "G-7Z31CNXTCK"
+};
+
+
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const projectStorage = firebase.storage()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp, projectStorage }
